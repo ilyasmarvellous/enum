71a6dde93582bfd0a24aa3065eebcff10fb4fa8e
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 22:07:34 -0600

    Add project files.

---
+++ enum_ex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_ex
+{
+    class Program
+    {
+        public enum DayofWeek
+        {
+            Sunday=1 , Monday=2, Tuesday=3, Wednesday=4, Thursday=5, Friday=6, Saturday=7
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Sunday, DayofWeek.Sunday);
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Monday, DayofWeek.Monday);
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Tuesday, DayofWeek.Tuesday);
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Wednesday, DayofWeek.Wednesday);
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Thursday, DayofWeek.Thursday);
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Friday, DayofWeek.Friday);
+            Console.WriteLine("Day of week {0} {1}", (int)DayofWeek.Saturday, DayofWeek.Saturday);
+            Console.ReadLine();
+        }
+        }
+}
+
